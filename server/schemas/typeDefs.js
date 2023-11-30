83be2be8abe8f24be2364f00385b3fa1cd39ab4c
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-11-29 21:48:24 -0500

    added book input type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,6 +27,15 @@ const typeDefs = gql`
     me: User
   }
 
+  input BookInput{
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth

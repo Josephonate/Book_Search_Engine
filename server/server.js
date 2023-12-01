c29f51fa42b540c580bbae1c65f31d86d1a39b2a
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-11-30 22:26:06 -0500

    added mongodb into dependencies

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   db.once("open", () => {
     app.listen(PORT, () => {
-      console.log(`Server now running on port ${PORT}!`);
+      // console.log(`Server now running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
     });
   });
